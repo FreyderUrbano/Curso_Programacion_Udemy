9b65b21991c73fdfcc58193f4375fb3692ca06db
FreyderUrbano <freurbano@gmail.com>
2021-10-03 16:36:24 -0500

    update

--- Python/Leccion1/main.py
+++ Python/Leccion1/main.py
@@ -1,3 +1,25 @@
 #FUNCION PARA CREAR "HOLA MUNDO"
 
 print("HOLA MUNDO CON PYTHON")
+
+miVariable = "22222"
+print( miVariable)
+print(miVariable)
+print(miVariable)
+
+miVariable = 500000
+print(miVariable)
+
+x = 2000
+y = 2120
+
+z = x + y
+print(z)
+print(x)
+print(y)
+
+#FUNCION id SIRVE PARA SABER LA LOCALIZACION EN MEMORIA DE LAS VARIABLES
+
+print(id(x))
+print(id(y))
+print(id(z))
